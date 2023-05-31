c1909dca1312183ee45a103d5624484ac598be97
EORIN <povorovegor@yandex.ru>
2023-05-31 17:42:18 +0300

    initial

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 const express = require('express')
 const app = express()
-cosnt fs = require('fs')
+const fs = require('fs')
+const bp = require('body-parser')
+const {translate} = require('free-translate')
+app.use(bp.json())
+app.use(bp.urlencoded({extended: true}))
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.sendFile(`${__dirname}/form.html`)
 })
+app.post('/post', (req, res) => {
+    fs.writeFileSync(`${__dirname}/gpt.txt`, req.body.data)
+    fs.readFile(`${__dirname}/gpt_f.txt`, 'utf8', (err, data) => {
 
-app.post('/post', (req, res)=>{
-    res.send('OK!')
+        res.send(data)
+    })
+
+
 })
 
-app.listen('3333', ()=>{
+app.listen('3334', () => {
     console.log('i am working')
 })
